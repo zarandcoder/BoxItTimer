2c876ade78d88d7691a5e1b697a3a766d1c0148d
Vadim Zaripov <vadim.zaripov@pentasys.de>
2020-05-28 20:49:21 +0200

    Added new Functionality to the BoxItTimer

--- app/src/main/java/website/zaripov/boxittimer/CountActivity.java
+++ app/src/main/java/website/zaripov/boxittimer/CountActivity.java
@@ -1,13 +1,112 @@
 package website.zaripov.boxittimer;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class CountActivity implements View.OnClickListener {
+public class CountActivity extends AppCompatActivity {
+    private static String TAG = "xx Count Activity";
+    private Long millisLeft = null;
+    private Long millisOnPause = null;
 
 
+
+    public CountActivity() {}
 
     @Override
-    public void onClick(View v) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count);
 
+        //Boxing bell sound
+        final MediaPlayer bellSound = MediaPlayer.create(this, R.raw.sample);
+        final TextView timeCounter = findViewById(R.id.textView6);
+
+        Intent intent = getIntent();
+
+        final String fightTimeStr = intent.getStringExtra("fightTime");
+        final String fightTimeStrPretty = intent.getStringExtra("fightTimePretty");
+
+        final String restTimeStr = intent.getStringExtra("restTime");
+        final String restTimeStrPretty = intent.getStringExtra("restTimePretty");
+
+        final String roundStr = intent.getStringExtra("rounds");
+
+
+
+        timeCounter.setText(fightTimeStrPretty);
+
+        ImageButton playBtn = findViewById(R.id.imageButton2);
+        ImageButton pauseBtn = findViewById(R.id.imageButton);
+        ImageButton stopBtn = findViewById(R.id.imageButton3);
+
+        final CountDownTimer timer;
+        if(fightTimeStr != null) {
+
+            //If user press Pause Button we have to save the time
+            long fightTime;
+            if(millisOnPause == null) {
+               fightTime = Integer.parseInt(fightTimeStr) * 1000;
+            } else fightTime = millisOnPause;
+
+            timer = new CountDownTimer(fightTime,
+                    1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    //TODO Parse time as String
+                    millisLeft = millisUntilFinished;
+                    timeCounter.setText(String.valueOf(millisLeft / 1000));
+                }
+
+                @Override
+                public void onFinish() {
+                    TextView currentState = findViewById(R.id.textView5);
+                    currentState.setText(R.string.rest);
+                    bellSound.start();
+                }
+            };
+        } else throw new NullPointerException("Problem occured");
+
+
+        //Event listeners for 3 Buttons
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bellSound.start();
+                TextView currentState = findViewById(R.id.textView5);
+                currentState.setText(getString(R.string.fight));
+                timer.start();
+            }
+        });
+
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView currentState = findViewById(R.id.textView5);
+                millisOnPause = millisLeft;
+                timer.cancel();
+                currentState.setText(getString(R.string.time_paused));
+            }
+        });
+
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                timer.cancel();
+                Intent intent1 = new Intent(CountActivity.this, MainActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
--- app/src/main/java/website/zaripov/boxittimer/MainActivity.java
+++ app/src/main/java/website/zaripov/boxittimer/MainActivity.java
@@ -2,62 +2,91 @@ package website.zaripov.boxittimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = "xx Main Activity";
 
-    private EditText mfightTime;
-    private EditText mrestTime;
-    private EditText mnumRounds;
+    private EditText fightTimeText;
+    private EditText restTimeText;
+    private EditText roundsText;
 
-    private ImageButton mPlay;
-    private ImageButton mPause;
-    private ImageButton mStop;
+    private String fightTimePretty;
+    private String fightTime;
 
+    private String restTime;
+    private String restTimePretty;
 
+    private String rounds;
 
-    private int fightTime;
-    private int restTime;
-    private int numRounds;
-
+    public MainActivity() {}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mfightTime = this.findViewById(R.id.editText);
-        mrestTime = this.findViewById(R.id.editText2);
-        mnumRounds = this.findViewById(R.id.editText3);
+        fightTimeText = findViewById(R.id.editText);
+        restTimeText = findViewById(R.id.editText);
+        roundsText = findViewById(R.id.editText);
 
-        mPlay = (ImageButton) this.findViewById(R.id.imageButton2);
-        final MediaPlayer bing = MediaPlayer.create(this, R.raw.sample);
 
-        mPlay.setOnClickListener(new View.OnClickListener() {
 
+        Button startBtn = findViewById(R.id.button);
+        startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bing.start();
+                //Check values first
+                if(!valuesAreEmpty()) {
+
+                    fightTimePretty = parseMinsAndSecs(fightTimeText.getText().toString());
+                    restTimePretty = parseMinsAndSecs(fightTimeText.getText().toString());
+
+                    fightTime = fightTimeText.getText().toString();
+                    restTime = restTimeText.getText().toString();
+                    rounds = roundsText.getText().toString();
+
+                    Intent intent = new Intent(MainActivity.this, CountActivity.class);
+                    intent.putExtra("fightTime", fightTime);
+                    intent.putExtra("fightTimePretty", fightTimePretty);
+                    intent.putExtra("restTime", restTime);
+                    intent.putExtra("restTimePretty", restTimePretty);
+                    intent.putExtra("rounds", rounds);
+
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                              "Please set time correctrly",
+                                   Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
 
-        /*
-        fightTime = Integer.parseInt(mfightTime.getText().toString());
-        restTime = Integer.parseInt(mrestTime.getText().toString());
-        numRounds = Integer.parseInt(mnumRounds.getText().toString());
-        */
+    private String parseMinsAndSecs(String sec) {
+        int secInt = Integer.parseInt(sec);
+        String mins = String.valueOf((secInt % 3600) / 60);
+        String secs = String.valueOf(secInt % 60);
+        if(mins.length() < 2) {
+            mins = "0" + mins;
+        }
 
-        Log.d(TAG, mfightTime.getText().toString());
-        Log.d(TAG, mrestTime.getText().toString());
-        Log.d(TAG, mnumRounds.getText().toString());
+        if(secs.length() < 2) {
+            secs = "0" + secs;
+        }
+        return String.format("%s : %s", mins, secs);
+    }
 
-
-
+    private boolean valuesAreEmpty() {
+        return (fightTimeText.getText().toString().equalsIgnoreCase("") ||
+                restTimeText.getText().toString().equalsIgnoreCase("") ||
+                roundsText.getText().toString().equalsIgnoreCase(""));
     }
 }
